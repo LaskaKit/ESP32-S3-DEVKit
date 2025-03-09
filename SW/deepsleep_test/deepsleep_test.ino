096bcfb8577ed5876b3c1d13a17c2b3d88860a3f
Konstantin Láska <konstantin.laska@icloud.com>
2025-03-09 19:39:29 +0100

    update

--- SW/deepsleep_test/deepsleep_test.ino
+++ SW/deepsleep_test/deepsleep_test.ino
@@ -1,10 +1,14 @@
 // Must be disabled USB CDC On Boot
 
 #define SLEEP_SEC 15         // Measurement interval (seconds)
+#define POWER 47
 
 void setup(void)
 {
   Serial.begin(115200);
+  pinMode(POWER, OUTPUT); 
+  digitalWrite(POWER, HIGH); // enable power supply for ePaper and uSup
+  delay(500);
 
   goToSleep();
 }
@@ -19,9 +23,11 @@ void goToSleep(){
   Serial.println("I'm mot sleeping");
   delay(3000);
   Serial.println("going to sleep 15 sek");
+  digitalWrite(POWER, LOW); // disable power supply for ePaper
   // ESP Deep Sleep 
   Serial.println("ESP in sleep mode");
   Serial.flush(); 
   esp_sleep_enable_timer_wakeup(SLEEP_SEC * 1000000);
+  delay(200);
   esp_deep_sleep_start();
 }
--- Version.md
+++ Version.md
@@ -1,4 +1,8 @@
-### Version 1.1
+### Version 1.3
+- New power part, layout optimization | 
+#### Version 1.2
+- Adjustment of the IC1 switching | Upravá spinání IC1
+#### Version 1.1
 - D4 deleted | odstraněná dioda D4
 #### Version 1.0
 - Initial | Počáteční
