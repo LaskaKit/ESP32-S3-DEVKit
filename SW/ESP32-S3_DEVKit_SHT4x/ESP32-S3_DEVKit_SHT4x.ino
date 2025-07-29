650b11de4b8d912a8507ca193909cb32360cca2c
Konstantin Láska <konstantin.laska@icloud.com>
2025-07-29 09:22:40 +0200

    changed to adafruit library

--- SW/ESP32-S3_DEVKit_SHT4x/ESP32-S3_DEVKit_SHT4x.ino
+++ SW/ESP32-S3_DEVKit_SHT4x/ESP32-S3_DEVKit_SHT4x.ino
@@ -7,59 +7,92 @@
 *
 * Made by (c) laskakit.cz 2023
 *
-* Libraries: https://github.com/Sensirion/arduino-i2c-sht4x
 * 
 */
-
-#include <SensirionI2CSht4x.h>
+#include "Adafruit_SHT4x.h"
 
 #define PIN_ON 47
 
-SensirionI2CSht4x sht4x;
+
+Adafruit_SHT4x sht4 = Adafruit_SHT4x();
 
 void setup() {
   Serial.begin(115200);
+
+  while (!Serial)
+    delay(10);     // will pause Zero, Leonardo, etc until serial console opens
+
   pinMode(PIN_ON, OUTPUT);      // Set EN pin for second stabilisator as output
   digitalWrite(PIN_ON, HIGH);   // Turn on the second stabilisator
 
   Wire.begin(42, 2);            // set dedicated I2C pins 42 - SDA, 2 - SCL for ESP32-S3-DEVKit
 
-  uint16_t error;
-  char errorMessage[256];
-
-  sht4x.begin(Wire);
-
-  uint32_t serialNumber;
-  error = sht4x.serialNumber(serialNumber);
-  if (error) {
-      Serial.print("Error trying to execute serialNumber(): ");
-      errorToString(error, errorMessage, 256);
-      Serial.println(errorMessage);
-  } else {
-      Serial.print("Serial Number: ");
-      Serial.println(serialNumber);
+  Serial.println("Adafruit SHT4x test");
+  if (! sht4.begin()) {
+    Serial.println("Couldn't find SHT4x");
+    while (1) delay(1);
   }
+  Serial.println("Found SHT4x sensor");
+  Serial.print("Serial number 0x");
+  Serial.println(sht4.readSerial(), HEX);
+
+  // You can have 3 different precisions, higher precision takes longer
+  sht4.setPrecision(SHT4X_HIGH_PRECISION);
+  switch (sht4.getPrecision()) {
+     case SHT4X_HIGH_PRECISION: 
+       Serial.println("High precision");
+       break;
+     case SHT4X_MED_PRECISION: 
+       Serial.println("Med precision");
+       break;
+     case SHT4X_LOW_PRECISION: 
+       Serial.println("Low precision");
+       break;
+  }
+
+  // You can have 6 different heater settings
+  // higher heat and longer times uses more power
+  // and reads will take longer too!
+  sht4.setHeater(SHT4X_NO_HEATER);
+  switch (sht4.getHeater()) {
+     case SHT4X_NO_HEATER: 
+       Serial.println("No heater");
+       break;
+     case SHT4X_HIGH_HEATER_1S: 
+       Serial.println("High heat for 1 second");
+       break;
+     case SHT4X_HIGH_HEATER_100MS: 
+       Serial.println("High heat for 0.1 second");
+       break;
+     case SHT4X_MED_HEATER_1S: 
+       Serial.println("Medium heat for 1 second");
+       break;
+     case SHT4X_MED_HEATER_100MS: 
+       Serial.println("Medium heat for 0.1 second");
+       break;
+     case SHT4X_LOW_HEATER_1S: 
+       Serial.println("Low heat for 1 second");
+       break;
+     case SHT4X_LOW_HEATER_100MS: 
+       Serial.println("Low heat for 0.1 second");
+       break;
+  }
+  
 }
 
-void loop() {
-  uint16_t error;
-  char errorMessage[256];
-  float temperature;
-  float humidity;
-  
-  error = sht4x.measureHighPrecision(temperature, humidity);
-  if (error) {
-      Serial.print("Error trying to execute measureHighPrecision(): ");
-      errorToString(error, errorMessage, 256);
-      Serial.println(errorMessage);
-  } else {
-      Serial.print("Temperature:");
-      Serial.print(temperature);
-      Serial.print("\t");
-      Serial.print("Humidity:");
-      Serial.println(humidity);
-  }
-  
-  delay(1000);
 
+void loop() {
+  sensors_event_t humidity, temp;
+  
+  uint32_t timestamp = millis();
+  sht4.getEvent(&humidity, &temp);// populate temp and humidity objects with fresh data
+  timestamp = millis() - timestamp;
+
+  Serial.print("Temperature: "); Serial.print(temp.temperature); Serial.println(" degrees C");
+  Serial.print("Humidity: "); Serial.print(humidity.relative_humidity); Serial.println("% rH");
+
+  Serial.print("Read duration (ms): ");
+  Serial.println(timestamp);
+
+  delay(1000);
 }
